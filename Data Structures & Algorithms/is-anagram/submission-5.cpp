40bf4c143c73c4ea1110a1bc033481287e25bb29
Ritik Jangid <ritikjangid194@gmail.com>
2026-09-26 23:30:29 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        vector<int>vec(26,0);
+        for(int i=0;i<s.length();i++){
+            vec[s[i]-'a']++;
+            vec[t[i]-'a']--;
+
+        }
+        for(int x:vec){
+        if(x!=0){
+            return false;
+        }
+        }
+        return true;
+    }
+};
